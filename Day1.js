68a15f216ecaf0333a0f0985bde6f5b4746b9d8e
Mohd-Salman19 <mohdsalman25555@gmail.com>
2026-01-01 23:46:31 +0530

    Add Day1.js with JavaScript practice code

---
+++ Day1.js
@@ -0,0 +1,43 @@
+// JavaScript Day 1 Practice
+console.log("Learning JavaScript");
+console.log("Apna College");
+
+// Variables
+let name = "Salman";
+let age = 20;
+const country = "India";
+
+console.log(name);
+console.log(age);
+console.log(country);
+
+// Data Types
+let isStudent = true;
+let x;
+let y = null;
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(x);
+console.log(y);
+
+// Practice Question 1
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    price: 270,
+    offer: 5
+};
+
+console.log(product);
+
+// Practice Question 2
+const profile = {
+    name: "Salman",
+    isFollow: true,
+    followers: 100,
+    following: 50
+};
+
+console.log(profile);
