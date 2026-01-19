69e4603939c342b23bdc297f55038a0cc99b917d
mohammad salman <mohdsalman25555@gmail.com>
2026-01-19 23:57:49 +0530

    Add program to check if a number is even or odd

---
+++ even-odd.js
@@ -0,0 +1,9 @@
+// Program to check Even or Odd
+
+let number = 7;   // you can change this number
+
+if (number % 2 === 0) {
+    console.log(number + " is Even");
+} else {
+    console.log(number + " is Odd");
+}
