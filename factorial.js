c879b8ac33eccef6a258dd99e3cd286ff9696e42
mohammad salman <mohdsalman25555@gmail.com>
2026-01-19 23:58:31 +0530

    Add factorial calculation program
    This program calculates the factorial of a given number.

---
+++ factorial.js
@@ -0,0 +1,10 @@
+// Program to find Factorial of a number
+
+let num = 5;   // you can change this number
+let factorial = 1;
+
+for (let i = 1; i <= num; i++) {
+    factorial = factorial * i;
+}
+
+console.log("Factorial of " + num + " is " + factorial);
