77d5be85fd0eeba16e4335a0f2ec91c777e3c29a
mohammad salman <mohdsalman25555@gmail.com>
2026-01-20 23:29:41 +0530

    Add program to calculate sum of array elements

---
+++ day4/array-sum.js
@@ -0,0 +1,11 @@
+// Program to find sum of array elements
+
+let arr = [10, 20, 30, 40, 50];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+}
+
+console.log("Array:", arr);
+console.log("Sum of array:", sum);
