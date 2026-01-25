e8a8b3015e53a2984b3101cb0beae05b5e2066a0
mohammad salman <mohdsalman25555@gmail.com>
2026-01-25 23:40:29 +0530

    Add reverse array program

---
+++ day8/day8/reverse-array.js
@@ -0,0 +1,11 @@
+// Program to reverse an array
+
+let arr = [10, 20, 30, 40, 50];
+let reversed = [];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i]);
+}
+
+console.log("Original Array:", arr);
+console.log("Reversed Array:", reversed);
