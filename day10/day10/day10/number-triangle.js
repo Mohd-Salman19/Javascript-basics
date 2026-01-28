8a87ff9b9951138f63ababf1a4715473612a6a98
mohammad salman <mohdsalman25555@gmail.com>
2026-01-28 23:41:29 +0530

    Add number triangle pattern generator

---
+++ day10/day10/day10/number-triangle.js
@@ -0,0 +1,13 @@
+// Number Triangle Pattern
+
+let rows = 5;
+let pattern = "";
+
+for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= i; j++) {
+        pattern += j + " ";
+    }
+    pattern += "\n";
+}
+
+console.log(pattern);
