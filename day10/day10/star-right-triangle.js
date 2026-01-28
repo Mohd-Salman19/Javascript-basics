96291ca05af9768d076175a84fb6cb6e0bf7ca8f
mohammad salman <mohdsalman25555@gmail.com>
2026-01-28 23:40:37 +0530

    Add star right triangle pattern generator

---
+++ day10/day10/star-right-triangle.js
@@ -0,0 +1,13 @@
+// Star Right Triangle Pattern
+
+let rows = 5;
+let pattern = "";
+
+for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= i; j++) {
+        pattern += "* ";
+    }
+    pattern += "\n";
+}
+
+console.log(pattern);
