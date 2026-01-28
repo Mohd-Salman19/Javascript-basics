4dfbb77a773f8f5155593404f3f36a447902f4ba
mohammad salman <mohdsalman25555@gmail.com>
2026-01-29 00:49:48 +0530

    Add array declaration and basic operations

---
+++ day11/array-basics.js
@@ -0,0 +1,13 @@
+// Array declaration
+let numbers = [10, 20, 30, 40];
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(numbers);
+console.log(fruits);
+
+// Access elements
+console.log(numbers[0]);
+console.log(fruits[1]);
+
+// Array length
+console.log("Length:", numbers.length);
