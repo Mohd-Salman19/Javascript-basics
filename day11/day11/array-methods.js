68f16a39a9b91fab74cc0c8bf331110321ca6860
mohammad salman <mohdsalman25555@gmail.com>
2026-01-29 00:51:02 +0530

    Add array methods demonstration in array-methods.js

---
+++ day11/day11/array-methods.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3];
+
+// push
+arr.push(4);
+console.log("After push:", arr);
+
+// pop
+arr.pop();
+console.log("After pop:", arr);
+
+// unshift
+arr.unshift(0);
+console.log("After unshift:", arr);
+
+// shift
+arr.shift();
+console.log("After shift:", arr);
