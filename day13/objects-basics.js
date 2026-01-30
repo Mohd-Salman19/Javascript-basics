e32c9715f24b9a41033169d0f4b7d10aa99bae37
mohammad salman <mohdsalman25555@gmail.com>
2026-01-31 00:13:28 +0530

    Create objects-basics.js

---
+++ day13/objects-basics.js
@@ -0,0 +1,12 @@
+// Creating an object
+let student = {
+  name: "Rahul",
+  age: 20,
+  course: "CSE"
+};
+
+console.log(student);
+
+// Access properties
+console.log(student.name);
+console.log(student["age"]);
