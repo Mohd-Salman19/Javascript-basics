cd90060feffcccfd65b40c394b9053e223227e6c
mohammad salman <mohdsalman25555@gmail.com>
2026-02-26 23:51:22 +0530

    Create script.js

---
+++ Day-35-Digital-Clock/script.js
@@ -0,0 +1,23 @@
+function updateClock(){
+
+    let now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    let day = now.toDateString();
+
+    if(hours < 10) hours = "0" + hours;
+    if(minutes < 10) minutes = "0" + minutes;
+    if(seconds < 10) seconds = "0" + seconds;
+
+    document.getElementById("clock").innerText =
+    hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("date").innerText = day;
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
